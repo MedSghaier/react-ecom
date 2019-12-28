f4d638cc4167034c39d86d13ff61ae3663d4ca7d
mohamed <mohamed@intuitiv-interactive.com>
2019-12-28 13:38:11 +0100

    removed firebase config creds for secuity

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyCWcXpEtPZM7UFmAjRvW6pRvGWouuu1rFQ",
-    authDomain: "ecom-clothing-store.firebaseapp.com",
-    databaseURL: "https://ecom-clothing-store.firebaseio.com",
-    projectId: "ecom-clothing-store",
-    storageBucket: "ecom-clothing-store.appspot.com",
-    messagingSenderId: "125601101688",
-    appId: "1:125601101688:web:1008560b301f1f3e7dfa8d"
+   
 }
 
 
